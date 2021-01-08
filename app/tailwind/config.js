9d142a2548108f01160f8fc27ed56d036748b500
achraf <achrafatauil@hotmail.com>
2021-01-08 14:48:48 +0100

    add colors, container padding  & grid template to tailwind config

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -2,10 +2,34 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      white: '#FFFFFF',
+      gray: {
+        darkest: '#151515',
+        dark: '#222222',
+        DEFAULT: '#505050',
+        light: '#C8C8C8',
+        lightest: '#f9fafc',
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      gridTemplateColumns: {
+        'content': '1fr 100px 1fr',
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
 }
