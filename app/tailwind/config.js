a8dc6684eae6d13a3e93a2738729fcda991667b6
achraf <achrafatauil@hotmail.com>
2021-01-31 17:05:03 +0100

    remove custom colors tailwind and use defaults in config.js

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -2,20 +2,6 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      white: '#FFFFFF',
-      black: '#000000',
-      gray: {
-        darkest: '#141414',
-        dark: '#222222',
-        DEFAULT: '#505050',
-        light: '#C8C8C8',
-        lightest: '#f5f5f5',
-      },
-      red: {
-        DEFAULT: '#CC0000'
-      },
-    },
     container: {
       center: true,
       padding: {
