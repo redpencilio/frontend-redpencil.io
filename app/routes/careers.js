e2a36fd2ab43f6bd7e0ad1c08799464dbacc630b
achraf <achrafatauil@hotmail.com>
2021-01-31 17:03:48 +0100

    generate careers route

--- app/routes/careers.js
+++ app/routes/careers.js
@@ -1,10 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class CareersRoute extends Route {
-  breadCrumbs = [
-    {
-      label: "Careers",
-      route: "careers"
-    }
-  ]
+
 }
