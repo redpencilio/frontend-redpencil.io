46c6a6bd525214336b6d7ca33848be0a10d9b011
Unknown <achrafatauil@hotmail.com>
2021-02-05 11:49:10 +0100

    add logic to close menu on route change

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,10 +1,19 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class NavbarComponent extends Component {
-
+  @service router;
   @tracked active = false;
+  @tracked currentRoute = this.router.currentRouteName
+
+  constructor(){
+    super(...arguments);
+    this.router.addObserver('currentRoute', () => {
+      this.toggleActive(false);
+    });
+  }
 
   @action toggleActive(state){
     if(typeof state == String){
