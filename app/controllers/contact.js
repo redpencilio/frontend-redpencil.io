d1f85bef28f01e3a35939cb986e21dc5d9c2c527
Unknown <achrafatauil@hotmail.com>
2021-02-09 17:16:51 +0100

    add simple try catch block

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -12,21 +12,25 @@ export default class ContactController extends Controller {
   @action
     async sendEmail(e) {
       e.preventDefault()
-      const mailFolder = await this.store.findRecord('mail-folder', "786a8cb0-4452-40f5-b8d8-046dd2d63281")
-      const newEmail = this.store.createRecord('email', {
-        to: "meettheteam@redpencil.io",
-        subject: "New form submission (rpio-website)",
-        from: "meettheteam@redpencil.io",
-        content: `
-          name: ${this.name}
-          email: ${this.email}
-          phone: ${this.phone}
-          message: ${this.message}
-        `,
-        folder: mailFolder
-      })
+      try {
+        const mailFolder = await this.store.findRecord('mail-folder', "786a8cb0-4452-40f5-b8d8-046dd2d63281")
+        const newEmail = this.store.createRecord('email', {
+          to: "meettheteam@redpencil.io",
+          subject: "New form submission (rpio-website)",
+          from: "meettheteam@redpencil.io",
+          content: `
+            name: ${this.name}
+            email: ${this.email}
+            phone: ${this.phone}
+            message: ${this.message}
+          `,
+          folder: mailFolder
+        })
+        newEmail.save()
+      } catch(err){
+        console.log(err)
+      }
 
-      newEmail.save()
 
 
     }
