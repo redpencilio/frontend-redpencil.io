c55797505e99b97f66125528d4d8ab2ef7fdadb1
Unknown <achrafatauil@hotmail.com>
2021-02-17 10:20:45 +0100

    add carousel loop

--- app/components/hero/carousel.js
+++ app/components/hero/carousel.js
@@ -7,8 +7,20 @@ export default class HeroCarouselComponent extends Component {
   @tracked index = 0;
   @tracked items = [0, 1, 2, 3, 4]
 
+  constructor(){
+    super(...arguments);
+    this.loopIndex()
+  }
+
   @action
   setIndex(position){
     this.index = position
+  }
+
+  loopIndex(){
+    const nextIndex = () => {
+      this.index = (this.index + 1) % this.items.length
+    }
+    setInterval(nextIndex, 7000);
   }
 }
