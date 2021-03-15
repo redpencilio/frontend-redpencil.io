13de7828d7526762c25030e5a515738441a217e0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-15 12:24:15 +0100

    Remove the observer usage in the NavBar component

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -6,20 +6,23 @@ import { inject as service } from '@ember/service';
 export default class NavbarComponent extends Component {
   @service router;
   @tracked active = false;
-  @tracked currentRoute = this.router.currentRouteName;
 
   constructor() {
     super(...arguments);
-    this.router.addObserver('currentRoute', () => {
-      this.toggleActive('false');
-    });
+    this.router.on('routeWillChange', this.closeMobileMenu);
   }
 
-  @action toggleActive(state) {
-    if (state == 'false') {
-      this.active = false;
-    } else {
-      this.active = !this.active;
-    }
+  willDestroy(...args) {
+    super.willDestroy(...args);
+
+    this.router.off('routeWillChange', this.closeMobileMenu);
+  }
+
+  @action closeMobileMenu() {
+    this.active = false;
+  }
+
+  @action toggleActive() {
+    this.active = !this.active;
   }
 }
