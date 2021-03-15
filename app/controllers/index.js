f2ce8186b01d50bd7a5023b5e237de82306c105a
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-15 12:05:46 +0100

    Remove the index controller
    It isn't being used and caused linting errors.

--- app/controllers/index.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-import { action } from '@ember/object';
-
-export default class IndexController extends Controller {
-  constructor() {
-    super(...arguments);
-  }
-}
