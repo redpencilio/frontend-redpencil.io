48651e751ec810774849b49c2717166ac82bd645
Erika Pauwels <erika.pauwels@gmail.com>
2021-12-26 23:01:46 +0100

    Update package.json metadata

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'rpio-website/config/environment';
+import config from 'frontend-redpencil/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
