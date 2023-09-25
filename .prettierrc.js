cc380683fcfe11db76552906ddc2e2edb6461caf
Achraf Atauil <achrafatauil@hotmail.com>
2023-09-25 20:52:26 +0200

    upgrade ember -> v4.12.1

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
   "plugins": ["prettier-plugin-tailwindcss"]
 };
