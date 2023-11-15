27d8233dedcc4bb51df1b5164d6cc26780461443
Erika Pauwels <erika.pauwels@gmail.com>
2023-11-15 14:54:18 +0100

    Disable CSS minification during builds

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    minifyCSS: {
+      enabled: false
+    },
   });
 
   const { Webpack } = require('@embroider/webpack');
