731bc5b6672368e2e0983e13b8242c9d100cb417
Niels Vandekeybus <progster@gmail.com>
2025-09-02 15:38:06 +0200

    fix sending of contact form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class ContactController extends Controller {
+  @service store;
   @tracked name;
   @tracked email;
   @tracked phone;
